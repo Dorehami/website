c31ef5d7fb8c9049c2ef17717e2ecb29b3487805
Aien <aien.saidi@gmail.com>
2025-03-31 09:29:15 +0200

    feat: reset migrations

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -12,6 +12,7 @@ use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[ORM\Table(name: '`user`')]
+#[ORM\HasLifecycleCallbacks]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
@@ -62,11 +63,20 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\JoinColumn(nullable: true)]
     private ?User $bannedBy = null;
 
+    #[ORM\Column(nullable: true)]
+    private ?DateTimeImmutable $joinedAt = null;
+
     public function __construct()
     {
         $this->posts = new ArrayCollection();
         $this->comments = new ArrayCollection();
         $this->postVotes = new ArrayCollection();
+    }
+
+    #[ORM\PrePersist]
+    public function prePersist(): void
+    {
+        $this->joinedAt = new DateTimeImmutable();
     }
 
     public function getId(): ?int
@@ -326,5 +336,17 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     public function __toString(): string
     {
         return $this->getDisplayName();
+    }
+
+    public function getJoinedAt(): ?DateTimeImmutable
+    {
+        return $this->joinedAt;
+    }
+
+    public function setJoinedAt(?DateTimeImmutable $joinedAt): static
+    {
+        $this->joinedAt = $joinedAt;
+
+        return $this;
     }
 }
